ec1883808f007c3701efc63f37a3177e28afa244
Liz Burke <liz.burke.chi@gmail.com>
2021-01-09 21:13:28 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -9,6 +9,11 @@ def toadstool
 end
 
 def link
-  phrase = "It's Dangerous To Go Alone! Take This."
-  puts phrase
+  link_phrase = "It's Dangerous To Go Alone! Take This."
+  puts link_phrase
+end
+
+def all_phrases
+  
+  
 end
